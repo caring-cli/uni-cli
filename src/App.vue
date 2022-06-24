546f81416b54ebceb0551d90c94726feaf43a058
风度万人迷 <805391114@qq.com>
2022-06-24 09:57:19 +0800

    version:0.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script>
 	export default {
-		onLaunch: function() {
+		onLaunch () {
 			console.log('App Launch')
 		},
-		onShow: function() {
+		onShow () {
 			console.log('App Show')
 		},
-		onHide: function() {
+		onHide () {
 			console.log('App Hide')
 		}
 	}
@@ -14,4 +14,5 @@
 
 <style lang="scss">
 	/*每个页面公共css */
+
 </style>
